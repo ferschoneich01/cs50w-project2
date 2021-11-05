142f27db0e2ff8ef96c68ef1abdab8a602a32e3b
ferschoneich01 <ferschoneich@gmail.com>
2021-11-05 08:55:50 +0600

    Chat

--- application.py
+++ application.py
@@ -1,20 +1,134 @@
 import os
 
-from flask import Flask, render_template, url_for
-from flask_socketio import SocketIO, emit
+from flask import Flask, render_template, url_for,request,flash, redirect, session
+from flask_socketio import SocketIO, emit,send,leave_room,join_room
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from funciones import *
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-socketio = SocketIO(app)
 
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+if not os.getenv("SECRET_KEY"):
+    raise RuntimeError("SECRET_KEY is not set")
+
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY") or 'Secret'
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_PERMANENT']= False
+Session(app)
+socketio = SocketIO(app, manage_session=False)
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+# Recibirá los nuevos mensajes y los emitirá por socket.
+@socketio.on('message') # recibir msj del lado del cliente al servidor (EVENTO) . 
+def handle_Message(msg): # Comenzamos a manejar el msj.
+    #print('Mensaje: ' + msg + 'Username: '+userName) #mensaje en terminal.
+    send(msg, broadcast = True) #mensaje al lado del cliente con su transmision.
+
+@socketio.on('left') # recibir msj del lado del cliente al servidor (EVENTO) . 
+def left_Message():
+    redirect("/") 
 
 @app.route("/")
+@login_required
 def index():
-    friends=[]
-    friends.append(['https://www.inboundcycle.com/hubfs/ClaraAlvarez.jpg','Alejandra Torres'])
-    friends.append(['https://www.trecebits.com/wp-content/uploads/2019/02/Persona-1-445x445.jpg','Who Lee'])
-    return render_template("index.html", friends=friends)
+    return render_template("index.html")
 
-@app.route("/chat")
-def chat():
-    return render_template("chat.html")
+
+@app.route("/login", methods=["POST","GET"])
+def login():
+    # Forget any user_id
+    session.clear()
+
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            print('Ingrese un nombre de usuario')
+            return redirect("/login")
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            print('Ingrese una contraseña')
+            return redirect("/login")
+
+        #nomrbre de usuario ingresado
+        username = request.form.get("username")
+        
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = '"+username+"'").fetchall()
+
+        if len(rows) != 1 or not check_password_hash(rows[0]["password"], request.form.get("password")):
+            print('Contraseña Incorrecta')
+            return redirect("/login")
+
+
+        # Remember which user has logged in
+        session["id_user"] = rows[0]["id_user"]
+        user = db.execute("SELECT * FROM users WHERE username = '"+username+"'").fetchall() 
+        username = user[0]["fullname"]
+        
+        photo = user[0]["image"]
+        friends=[]
+        friends.append(['https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Noun_Project_Community_icon_986471.svg/1024px-Noun_Project_Community_icon_986471.svg.png','Community Moon Message'])
+        # Redirect user to home page
+        return render_template("index.html",username=username,photo=photo,friends=friends)
+    else:
+        return render_template("login.html")
+
+@app.route("/register", methods=["POST","GET"])
+def register():
+    # Forget any user_id
+    session.clear()
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            flash("Ingrese un nombre de usuario")
+            
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            flash("Ingrese una contraseña")
+
+        elif not request.form.get("fullname"):
+            flash("Ingrese un fullname")
+
+        elif not request.form.get("email"):
+            flash("Ingrese un email")
+
+        elif not request.form.get("sex"):
+            flash("Ingrese un sexo")
+        
+        elif not request.form.get("image"):
+            flash("Ingrese una imagen")
+
+        #nomrbre de usuario ingresado
+        username = request.form.get("username")
+        password = generate_password_hash(request.form.get("password"))
+        fullname = request.form.get("fullname")
+        email = request.form.get("email")
+        sex = request.form.get("select")
+        image = request.form.get("image")
+
+        print(username)
+        print(password)
+        print(fullname)
+        print(email)
+        print(image)
+        
+        # Query database for username
+        db.execute("INSERT INTO users (username,password,fullname,email,sex,image) VALUES ('"+str(username)+"','"+str(password)+"','"+str(fullname)+"','"+str(email)+"','"+str(sex)+"','"+str(image)+"')")
+        db.commit()
+        # Redirect user to home page
+        
+        return redirect("/")
+    else:
+        return render_template("register.html")
--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,105 @@
 
-function MostrarChat(name,img){
-        $("#Username-message").text(name);           
-        $("#img-message").attr("src",img);
-        $("#message").children().show();
-}
-window.onpageshow = function () {
+let hoy = new Date();
 
-        var ancho = window.innerWidth;
+document.addEventListener('DOMContentLoaded', () => {
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+    socket.on('connect', () => {
+
+        socket.on('message', function (msg) { //la conexion del cliente a servidor.
+            var hour = hoy.getHours() + ':' + hoy.getMinutes();
+            if (localStorage.getItem("username") == $('#username').val()) {
+                $('#texto-msj').append('<li style="list-style:none; widht:100%; margin:5px; height:auto;"><div style="float:right; max-width:95%; min-width:15%;  padding-left:5px; font-family: Arial, Helvetica, sans-serif; background:#ffff;  height:auto; border-radius:5px;">' + '<div style="color:green;">' +"You"+ '</div> <div style="margin-right:5px;">' + msg + '</div><div style="color:gray; font-size:8px; margin-right:5px;">'+hour+'</div></div></li>'); // insertamos el contenido de a la lista, es decir el texto-msj.
+
+            } else {
+                $('#texto-msj').append('<li style="list-style:none; margin:5px; widht:100%; height:auto;"><div style="float:left; max-width:95%; min-width:15%; padding-left:5px; font-family: Arial, Helvetica, sans-serif; background:#ffff; height:auto; border-radius:5px;">' + '<div style="color:blue;">' +localStorage.getItem("username")+ '</div><div style="margin-right:5px;">' + msg + '</div><div style="color:gray; font-size:8px; margin-right:5px;">'+hour+'</div></div></li>'); // insertamos el contenido de a la lista, es decir el texto-msj.
+            }
+        });
+
+        $('#Enviar').on('click', function () { //nuestro boton tendra la funcion al momento de clikear de mandar ese contenido a texto-msj
+            socket.send($('#mi-msj').val());
+            $('#mi-msj').val('');
+        });
+
+        $('#Salir').on('click', function (){
+            socket.emit('left', {}, function () {
+                socket.disconnect();
+                // go back to the login page
+                
+            });
+            cerrarMessages();
+        });
+
+       
         
-    if(ancho < 600){
-        $("#message").hide();    
+
+    });
+
+});
+
+function setUsername(user) {
+    localStorage.setItem("username",user);
+    var hour = hoy.getHours() + ':' + hoy.getMinutes();
+    var newMessage = {
+        username:user,
+        msg: $('#mi-msj').val(),
+        group: $('#Username-message').val(),
+        hour: hour
+    };
+
+    console.log(newMessage);
+    MsgList.push(newMessage);
+    setMessage(MsgList);
+    
+}
+
+function getMessage(){
+    var storedList = localStorage.getItem('MessageList');
+    if(storedList == null){
+        MsgList = [];
+
     }else{
-        $("#message").children().hide(); 
+        MsgList = JSON.parse(storedList);
     }    
-      
+    return MsgList;
+}
+
+function setMessage(newMessage){
+    localStorage.setItem('MessageList', JSON.stringify(newMessage));
+}
+
+function MostrarChat(name, img, group) {
+    $("#Username-message").text(name);
+    $("#img-message").attr("src", img);
+    var list = getMessage();
+    for(var i = 0; i < list.length; i++){
+        if(list[i].hour == ""){
+            list[i].hour = hoy.getHours() + ':' + hoy.getMinutes();
+        }
+        if (list[i].username == $('#username').val() && list[i].group == group) {
+            $('#texto-msj').append('<li style="list-style:none; widht:100%; margin:5px; height:auto;"><div style="float:right; max-width:95%; min-width:15%;  padding-left:5px; padding-right:5px; font-family: Arial, Helvetica, sans-serif; background:#ffff;  height:auto; border-radius:5px;">' + '<div style="color:green;">' +"You"+ '</div> <div style="margin-right:5px;">' + list[i].msg + '</div><div style="float:right; color:gray; font-size:10px; margin-top:5px; margin-right:5px;">'+list[i].hour+'</div></div></li>'); // insertamos el contenido de a la lista, es decir el texto-msj.
+    
+        } else if(list[i].group == group){
+            $('#texto-msj').append('<li style="list-style:none; margin:5px; widht:100%; height:auto;"><div style="float:left; max-width:95%; min-width:15%; padding-left:5px; padding-right:5px; font-family: Arial, Helvetica, sans-serif; background:#ffff; height:auto; border-radius:5px;">' + '<div style="color:blue;">' + list[i].username + '</div> <div style="margin-right:5px;">' + list[i].msg + '</div><div style="float:right; color:gray; font-size:10px; margin-top:5px; margin-right:5px;">'+list[i].hour+'</div></div></li>'); // insertamos el contenido de a la lista, es decir el texto-msj.
+        }
+       
+    }
+    $("#message").children().show();
+
+}
+
+window.onpageshow = function () {
+    //localStorage.removeItem('MessageList');
+    cerrarMessages();
+}
+
+function cerrarMessages(){
+    var ancho = window.innerWidth;
+
+    if (ancho < 600) {
+        $("#message").hide();
+    } else {
+        $("#message").children().hide();
+    }
+
+    $('#texto-msj').children().hide();
 }
