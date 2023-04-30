74de2115387c89b8d5b6b683140698c62dbea191
ferschoneich01 <59126953+ferschoneich01@users.noreply.github.com>
2023-04-30 00:45:22 +0000

    Sun, Apr 30, 2023, 6:45 AM +06:00

--- src/application.py
+++ src/application.py
@@ -179,7 +179,6 @@ def logout():
 @app.route("/register", methods=["POST", "GET"])
 def register():
     # Forget any user_id
-    session.clear()
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
 
